cc0820d8d5e0fee41ad4d2af1255a417a538ba38
anassfoughali <foughalianass09@gmail.com>
2024-05-15 16:21:55 +0100

    rspnsive design

--- AdminPannel/src/Header.jsx
+++ AdminPannel/src/Header.jsx
@@ -1,40 +1,17 @@
-// Header.js
 import React from 'react';
-import { CgUserList } from "react-icons/cg"
-import Button from './Button'; // Import your Button component
-
-const Header = () => {
-  // Define onClick handlers for your buttons
-  const handleButtonClick1 = () => {
-    // Handle button click logic for button 1
-  }
-
-  const handleButtonClick2 = () => {
-    // Handle button click logic for button 2
-  }
-
-  const handleButtonClick3 = () => {
-    // Handle button click logic for button 3
-  }
-
+import 
+ {BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify}
+ from 'react-icons/bs'
+ function Header({OpenSidebar}) {
   return (
-    <header >
-      <div className='Buttons' >
-        {/* Button 1 */}
-       
-
-        <Button  on Click={handleButtonClick1}   text="PRODUIT"  />
-      
-        {/* Button 2 */}
-
-        <Button onClick={handleButtonClick2} text="Team member" />
-        {/* Button 3 */}
-        <Button onClick={handleButtonClick3} text="03-06-2024" />
-      </div>
+    <header className='header'>
+        <div className='menu-icon'>
+            <BsJustify className='icon' onClick={OpenSidebar}/>
+        </div>
+        
+        
     </header>
-  );
-  
+  )
 }
-
 
 export default Header;
--- AdminPannel/src/Home.jsx
+++ AdminPannel/src/Home.jsx
@@ -4,90 +4,76 @@ import { CgMathPercent } from "react-icons/cg";
 import { CgUserList } from "react-icons/cg";
 import { CgPlayListCheck } from "react-icons/cg";
 import { TbBrandCodepen } from "react-icons/tb";
-import 
-{ BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillBellFill}
- from 'react-icons/bs'
+import { CgCalendar } from "react-icons/cg";
+import Button from './Button';
+import "bootstrap/dist/css/bootstrap.min.css"
+
  import { ResponsiveContainer, PieChart, Pie, Legend, BarChart,Bar,XAxis ,YAxis,CartesianGrid,Tooltip} from 'recharts';
- import Button from './Button';
+ 
  
  
 
 function Home() {
-  
+  const generateBarChartData = () => {
+    const currentDate = new Date();
+    const daysInMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    const data = [];
+
+    for (let i = 1; i <= daysInMonth; i++) {
+        data.push({ name: i, pv: Math.floor(Math.random() * 1000) }); // Random data for each day
+    }
+
+    return data;
+  };
 
  const data = [
-    {
-  
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
   { name: 'Group A', value: 400 },
   { name: 'Group B', value: 300 },
   { name: 'Group C', value: 300 },
   { name: 'Group D', value: 200 },
   ]
-
+  const barChartData = generateBarChartData();
   
     return(
       <main className='main-container'>
-         
- 
-
         <div className='main-title'> 
-          <h1>Overview</h1>
+          <h1 className='title-over'>Overview</h1>
+          <div className='buttons-style'>
+          <div className='Buttons'>
+          {/* Button 1 */}
+          <Button text="PRODUIT" />
+          {/* Button 2 */}
+          <Button text="Team member" />
+          {/* Button 3 */}
+          <Button text="03-06-2024" />
+        </div>
+        </div>
+      
+          
         </div >
         <div className='main-cards'>
           
           {/* card 1*/}
           <div className='row-div'>
           <div className='card-inner card1' >
-            <h3>Revenu net</h3>
+            <div>
+            <h4>Revenu net</h4>
+            <h6>$241.00</h6>
+            </div>
             <CgDollar className='card_icon'/>
           </div>
           <div className='card-inner card1'>
-            <h3>Total Command</h3>
+            <div>
+            <h4>Total Command</h4>
+            <h6>$241.00</h6>
+            </div>
             <CgDollar className='card_icon'/>
         </div>
         <div className='card-inner card1' >
-            <h3>TAUX DE CONVERSION</h3>
+          <div>
+            <h4>Taux De Conversion </h4>
+            <h6>$241.00</h6>
+            </div>
             <CgMathPercent className='card_icon'/>
           </div>
         </div>
@@ -95,15 +81,24 @@ function Home() {
         {/* card 2 */ }
         <div className='row-div'>
         <div className='card-inner card2' >
-            <h3>Chiffre D'affaire</h3>
+          <div>
+            <h4>Chiffre D'affaire</h4>
+            <h4>1</h4>
+            </div>
             <TbBrandCodepen className='card_icon'/>
           </div>
           <div className='card-inner card2'>
-            <h3>Commande expider</h3>
+            <div>
+            <h4>Commande expider</h4>
+            <h4>1</h4>
+            </div>
             <TbBrandCodepen className='card_icon'/>
         </div>  
         <div className='card-inner card2' >
-            <h3>TAUX D'expider</h3>
+          <div>
+            <h4>Taux D'expider</h4>
+            <h4>1</h4>
+            </div>
             <CgMathPercent className='card_icon'/>
           </div>
         </div>
@@ -112,40 +107,49 @@ function Home() {
         {/* card 3 */}
         <div className='row-div'>
         <div className='card-inner card3' >
-            <h3>Depense</h3>
+          <div>
+            <h4>Depense</h4>
+            <h4>2</h4>
+            </div>
             <CgUserList className='card_icon'/>
           </div>
           <div className='card-inner card3'>
-            <h3>Commande livre</h3>
+            <div>
+            <h4>Commande livre</h4>
+            <h4>2</h4>
+            </div>
             <CgPlayListCheck className='card_icon'/>
         </div>
         
         <div className='card-inner card3' >
-            <h3>TAUX De livre</h3>
+          <div>
+            <h4>Taux De livre</h4>
+            <h4>2</h4>
+            </div>
             <CgUserList className='card_icon'/>
           </div>
         
         </div>
-        
-        
-
-        
-        
-          </div>
-    
-
-       
        
       
+          </div>
+          
+          
         
-    
+          
         <div className='charts'>
+       
         <div className='style-chart'>
-        <ResponsiveContainer width="100%" height="100%">
+        <div className='chart-title'>
+          <h4>Statistique command<span className='date'>03-06-2024</span>  <CgCalendar className='clendar-icon'/>  </h4>  
+          
+        </div>
+        <ResponsiveContainer width="100%" height="80%">
+        
         <BarChart
           width={500}
           height={300}
-          data={data}
+          data={barChartData}
           margin={{
             top: 5,
             right: 30,
@@ -164,17 +168,56 @@ function Home() {
         </ResponsiveContainer>
         </div>
 
+
         
         <div className='style-chart'>
-      
-        <ResponsiveContainer>
-          <PieChart>
+        <div className='chart-title'>
+          <h4 className='chart-pie'>Statistiques Colis</h4>
+        </div>
+        <ResponsiveContainer width="100%" height="80%">
+          <PieChart className='piechart'>
             <Pie dataKey="value" data={data} fill="#0075ff" label />
           </PieChart>
         </ResponsiveContainer>
         </div>
       
         
+        </div>
+       <div className='table'>
+        <div className='table-title'><h3>Recent Sales</h3></div>
+        <table className="table table-striped" >
+                <thead>
+                    <tr>
+                        <th>Reference</th>
+                        <th>Customer</th>
+                        <th>Warehouse</th>
+                        <th>Status</th>
+                        <th>Grand Total</th>
+                        <th>Paid</th>
+                        <th>Due</th>
+                        <th>Payment Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>REF001</td>
+                        <td>Customer A</td>
+                        <td>Warehouse X</td>
+                        <td>Shipped</td>
+                        <td>$500</td>
+                        <td>$400</td>
+                    </tr>
+                    <tr>
+                        <td>REF002</td>
+                        <td>Customer B</td>
+                        <td>Warehouse Y</td>
+                        <td>Pending</td>
+                        <td>$700</td>
+                        <td>$0</td>
+                    </tr>
+                  
+                </tbody>
+            </table>
         </div>
        
       </main>
